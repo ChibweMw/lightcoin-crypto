51b6aa8fd44a8cba634b9b64a3a18a8c480c0a26
Chibwe Mweene <caleidosymmetry@gmail.com>
2018-06-05 22:54:05 +0000

    added accounts

--- index.js
+++ index.js
@@ -1,13 +1,33 @@
 let balance = 500.00;
 
-class Withdrawal {
+class Account {
 
-  constructor(amount) {
+  constructor(username) {
+    this.username = username;
+    this.balance = 0;
+  }
+}
+
+class Deposit {
+  constructor(amount, account) {
     this.amount = amount;
+    this.account = account;
   }
 
   commit() {
-    balance -= this.amount;
+    this.account.balance += this.amount;
+  }
+}
+
+class Withdrawal {
+
+  constructor(amount, account) {
+    this.amount = amount;
+    this.account = account;
+  }
+
+  commit() {
+    this.account.balance -= this.amount;
   }
 
 }
@@ -18,12 +38,18 @@ class Withdrawal {
 // DRIVER CODE BELOW
 // We use the code below to "drive" the application logic above and make sure it's working as expected
 
-t1 = new Withdrawal(50.25);
+const myAccount = new Account("snow-patrol");
+
+t1 = new Deposit(120.00, myAccount);
 t1.commit();
 console.log('Transaction 1:', t1);
 
-t2 = new Withdrawal(9.99);
+t2 = new Withdrawal(50.25, myAccount);
 t2.commit();
 console.log('Transaction 2:', t2);
 
-console.log('Balance:', balance);
+t3 = new Withdrawal(9.99, myAccount);
+t3.commit();
+console.log('Transaction 3:', t3);
+
+console.log('Current balance:', myAccount.balance);
